7dd52047cb6ae7e494e602c21f4af574bcd77d5a
JOHN\Anjoh <anjohnsun@gmail.com>
2024-07-15 00:58:33 +0300

    Matches detecting done

--- Assets/_Source/GridDrawer.cs
+++ Assets/_Source/GridDrawer.cs
@@ -1,25 +1,59 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using static UnityEngine.Rendering.DebugUI.Table;
 
 public class GridDrawer : MonoBehaviour
 {
-    private Tile[,] _tileGrid;
-    private GameIconsSO _gameIcons;
+    [SerializeField] private GameObject _tilePrefab;
+    [SerializeField] private GameIconsSO _gameIcons;
 
-    public void Construct(Tile[,] tileGrid, GameIconsSO gameIcons)
+    [SerializeField] private Vector2Int _size;
+    [SerializeField] private Vector2 _spacing;
+
+    [SerializeField] private Transform _anchor;
+
+    private Tile[,] _tiles;
+
+
+    private GridManager _gridManager;
+
+    void Start()
     {
-        _tileGrid = tileGrid;
-        _gameIcons = gameIcons;
+        _tiles = new Tile[_size.x, _size.y];
+        GenerateTiles();
+
+        _gridManager = new GridManager(_tiles, this, _gameIcons);
+
+        _gridManager.InitializeGrid();
+        RedrawGrid();
+        _gridManager.RemoveMatches();
     }
 
-    public void DrawGrid()
+    private void GenerateTiles()
     {
-        for (int i = 0; i < _tileGrid.GetLength(0); i++)
+        for (int i = 0; i < _size.x; i++)
         {
-            for (int j = 0; j < _tileGrid.GetLength(1); j++)
+            for (int j = 0; j < _size.y; j++)
             {
-                _tileGrid[i, j].ConnectedSprite.sprite = _gameIcons.Icons[_tileGrid[i, j].TileValue];
+                GameObject newTile = Instantiate(_tilePrefab);
+
+                newTile.transform.localPosition = _anchor.position + new Vector3(
+                    j + _spacing.x * j,
+                    i + _spacing.y * i);
+
+                _tiles[i, j] = newTile.GetComponent<Tile>();
+            }
+        }
+    }
+
+    public void RedrawGrid()
+    {
+        for (int i = 0; i < _tiles.GetLength(0); i++)
+        {
+            for (int j = 0; j < _tiles.GetLength(1); j++)
+            {
+                _tiles[i, j].ConnectedSprite.sprite = _gameIcons.Icons[_tiles[i, j].TileValue];
             }
         }
     }
--- Assets/_Source/GridManager.cs
+++ Assets/_Source/GridManager.cs
@@ -9,9 +9,10 @@ public class GridManager
     private int _iconCount;
 
 
-    public GridManager(Tile[,] tileGrid, GameIconsSO gameIcons)
+    public GridManager(Tile[,] tileGrid, GridDrawer gridDrawer, GameIconsSO gameIcons)
     {
         _tileGrid = tileGrid;
+        _gridDrawer = gridDrawer;
         _iconCount = gameIcons.Icons.Count;
     }
 
@@ -24,5 +25,116 @@ public class GridManager
                 _tileGrid[i, j].TileValue = Random.Range(0, _iconCount);
             }
         }
+    }
+
+    public List<List<Tile>> CheckMatches()
+    {
+        List<List<Tile>> matches = new List<List<Tile>>();
+
+        int currentType = -1;
+        int count = 0;
+
+        for (int i = 0; i < _tileGrid.GetLength(0); i++)
+        {
+            currentType = _tileGrid[i, 0].TileValue;
+            count = 1;
+
+            for (int j = 1; j < _tileGrid.GetLength(1); j++)
+            {
+                if (_tileGrid[i, j].TileValue == currentType)
+                    count++;
+                else
+                {
+                    if (count >= 3)
+                    {
+                        List<Tile> match = new List<Tile>();
+                        for (int x = j; x > j - count + 1; x--)
+                        {
+                            match.Add(_tileGrid[i, x]);
+                        }
+                        matches.Add(match);
+                    }
+                    count = 1;
+                    currentType = _tileGrid[i, j].TileValue;
+                }
+            }
+
+            if (count >= 3)
+            {
+                List<Tile> match = new List<Tile>();
+                for (int x = _tileGrid.GetLength(1)-1; x > _tileGrid.GetLength(1) - 1 - count + 1; x--)
+                {
+                    match.Add(_tileGrid[i, x]);
+                }
+                matches.Add(match);
+            }
+        }
+
+        for (int j = 0; j < _tileGrid.GetLength(1); j++)
+        {
+            currentType = _tileGrid[0, j].TileValue;
+            count = 1;
+
+            for (int i = 1; i < _tileGrid.GetLength(0); i++)
+            {
+                if (_tileGrid[i, j].TileValue == currentType)
+                    count++;
+                else
+                {
+                    if (count >= 3)
+                    {
+                        List<Tile> match = new List<Tile>();
+                        for (int x = i; x > i - count + 1; x--)
+                        {
+                            match.Add(_tileGrid[x, j]);
+                        }
+                        matches.Add(match);
+                    }
+                    count = 1;
+                    currentType = _tileGrid[i, j].TileValue;
+                }
+            }
+
+            if (count >= 3)
+            {
+                List<Tile> match = new List<Tile>();
+                for (int x = _tileGrid.GetLength(0) - 1; x > _tileGrid.GetLength(0) - 1 - count + 1; x--)
+                {
+                    match.Add(_tileGrid[x, j]);
+                }
+                matches.Add(match);
+            }
+        }
+
+        return matches;
+    }
+
+    public void RemoveMatches()
+    {
+        List<List<Tile>> matches = CheckMatches();
+        while (matches.Count != 0)
+        {
+            Debug.Log($"Matches found: {matches.Count}");
+            foreach (List<Tile> match in matches)
+            {
+                //
+            }
+            FillTiles(matches);
+
+            matches = CheckMatches();
+        }
+    }
+
+    private void FillTiles(List<List<Tile>> matches)
+    {
+        foreach(List<Tile> match in matches)
+        {
+            foreach(Tile tile in match)
+            {
+                tile.TileValue = Random.Range(0, _iconCount);
+            }
+        }
+
+        _gridDrawer.RedrawGrid();
     }
 }
